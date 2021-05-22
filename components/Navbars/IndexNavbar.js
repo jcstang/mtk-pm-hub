23c26a324890da39de961cc368d610a1c1256d89
Jacob Stanger <devjake@outlook.com>
2021-05-21 19:05:38 -0600

    headers

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -14,8 +14,8 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 // reactstrap components
 import {
   UncontrolledCollapse,
@@ -29,61 +29,61 @@ import {
   Col,
   UncontrolledTooltip,
   Button,
-} from "reactstrap";
+} from 'reactstrap';
 
 function AdminNavbar() {
   return (
     <>
       <Navbar
-        className="navbar-horizontal navbar-main navbar-dark bg-info"
-        expand="lg"
-        id="navbar-main"
+        className='navbar-horizontal navbar-main navbar-dark bg-info'
+        expand='lg'
+        id='navbar-main'
       >
         <Container>
-          <Link href="/">
+          <Link href='/'>
             <span>
-              <NavbarBrand href="#pablo">
+              <NavbarBrand href='#pablo'>
                 <img
-                  alt="..."
-                  src={require("assets/img/brand/nextjs_argon_white.png")}
+                  alt='...'
+                  src={require('assets/img/brand/nextjs_argon_white.png')}
                 />
               </NavbarBrand>
             </span>
           </Link>
           <button
-            aria-controls="navbar-collapse"
+            aria-controls='navbar-collapse'
             aria-expanded={false}
-            aria-label="Toggle navigation"
-            className="navbar-toggler"
-            data-target="#navbar-collapse"
-            data-toggle="collapse"
-            id="navbar-collapse"
-            type="button"
+            aria-label='Toggle navigation'
+            className='navbar-toggler'
+            data-target='#navbar-collapse'
+            data-toggle='collapse'
+            id='navbar-collapse'
+            type='button'
           >
-            <span className="navbar-toggler-icon" />
+            <span className='navbar-toggler-icon' />
           </button>
           <UncontrolledCollapse
-            className="navbar-custom-collapse"
+            className='navbar-custom-collapse'
             navbar
-            toggler="#navbar-collapse"
+            toggler='#navbar-collapse'
           >
-            <div className="navbar-collapse-header">
+            <div className='navbar-collapse-header'>
               <Row>
-                <Col className="collapse-brand" xs="6">
-                  <Link href="/admin/dashboard">
-                    <img alt="..." src={require("assets/img/brand/blue.png")} />
+                <Col className='collapse-brand' xs='6'>
+                  <Link href='/admin/dashboard'>
+                    <img alt='...' src={require('assets/img/brand/blue.png')} />
                   </Link>
                 </Col>
-                <Col className="collapse-close" xs="6">
+                <Col className='collapse-close' xs='6'>
                   <button
-                    aria-controls="navbar-collapse"
+                    aria-controls='navbar-collapse'
                     aria-expanded={false}
-                    aria-label="Toggle navigation"
-                    className="navbar-toggler"
-                    data-target="#navbar-collapse"
-                    data-toggle="collapse"
-                    id="navbar-collapse"
-                    type="button"
+                    aria-label='Toggle navigation'
+                    className='navbar-toggler'
+                    data-target='#navbar-collapse'
+                    data-toggle='collapse'
+                    id='navbar-collapse'
+                    type='button'
                   >
                     <span />
                     <span />
@@ -91,118 +91,118 @@ function AdminNavbar() {
                 </Col>
               </Row>
             </div>
-            <Nav className="mr-auto" navbar>
+            <Nav className='mr-auto' navbar>
               <NavItem>
-                <Link href="/admin/dashboard">
-                  <NavLink href="#pablo">
-                    <span className="nav-link-inner--text">Dashboard</span>
+                <Link href='/admin/dashboard'>
+                  <NavLink href='#pablo'>
+                    <span className='nav-link-inner--text'>Dashboardy</span>
                   </NavLink>
                 </Link>
               </NavItem>
               <NavItem>
-                <Link href="/auth/pricing">
-                  <NavLink href="#pablo">
-                    <span className="nav-link-inner--text">Pricing</span>
+                <Link href='/auth/pricing'>
+                  <NavLink href='#pablo'>
+                    <span className='nav-link-inner--text'>Pricing</span>
                   </NavLink>
                 </Link>
               </NavItem>
               <NavItem>
-                <Link href="/auth/login">
-                  <NavLink href="#pablo">
-                    <span className="nav-link-inner--text">Login</span>
+                <Link href='/auth/login'>
+                  <NavLink href='#pablo'>
+                    <span className='nav-link-inner--text'>Login</span>
                   </NavLink>
                 </Link>
               </NavItem>
               <NavItem>
-                <Link href="/auth/register">
-                  <NavLink href="#pablo">
-                    <span className="nav-link-inner--text">Register</span>
+                <Link href='/auth/register'>
+                  <NavLink href='#pablo'>
+                    <span className='nav-link-inner--text'>Register</span>
                   </NavLink>
                 </Link>
               </NavItem>
               <NavItem>
-                <Link href="/auth/lock">
-                  <NavLink href="#pablo">
-                    <span className="nav-link-inner--text">Lock</span>
+                <Link href='/auth/lock'>
+                  <NavLink href='#pablo'>
+                    <span className='nav-link-inner--text'>Lock</span>
                   </NavLink>
                 </Link>
               </NavItem>
             </Nav>
-            <hr className="d-lg-none" />
-            <Nav className="align-items-lg-center ml-lg-auto" navbar>
+            <hr className='d-lg-none' />
+            <Nav className='align-items-lg-center ml-lg-auto' navbar>
               <NavItem>
                 <NavLink
-                  className="nav-link-icon"
-                  href="https://www.facebook.com/creativetim?ref=creative-tim"
-                  id="tooltip601201423"
-                  target="_blank"
+                  className='nav-link-icon'
+                  href='https://www.facebook.com/creativetim?ref=creative-tim'
+                  id='tooltip601201423'
+                  target='_blank'
                 >
-                  <i className="fab fa-facebook-square" />
-                  <span className="nav-link-inner--text d-lg-none">
+                  <i className='fab fa-facebook-square' />
+                  <span className='nav-link-inner--text d-lg-none'>
                     Facebook
                   </span>
                 </NavLink>
-                <UncontrolledTooltip delay={0} target="tooltip601201423">
+                <UncontrolledTooltip delay={0} target='tooltip601201423'>
                   Like us on Facebook
                 </UncontrolledTooltip>
               </NavItem>
               <NavItem>
                 <NavLink
-                  className="nav-link-icon"
-                  href="https://www.instagram.com/creativetimofficial?ref=creative-tim"
-                  id="tooltip871243015"
-                  target="_blank"
+                  className='nav-link-icon'
+                  href='https://www.instagram.com/creativetimofficial?ref=creative-tim'
+                  id='tooltip871243015'
+                  target='_blank'
                 >
-                  <i className="fab fa-instagram" />
-                  <span className="nav-link-inner--text d-lg-none">
+                  <i className='fab fa-instagram' />
+                  <span className='nav-link-inner--text d-lg-none'>
                     Instagram
                   </span>
                 </NavLink>
-                <UncontrolledTooltip delay={0} target="tooltip871243015">
+                <UncontrolledTooltip delay={0} target='tooltip871243015'>
                   Follow us on Instagram
                 </UncontrolledTooltip>
               </NavItem>
               <NavItem>
                 <NavLink
-                  className="nav-link-icon"
-                  href="https://twitter.com/creativetim?ref=creative-tim"
-                  id="tooltip366258619"
-                  target="_blank"
+                  className='nav-link-icon'
+                  href='https://twitter.com/creativetim?ref=creative-tim'
+                  id='tooltip366258619'
+                  target='_blank'
                 >
-                  <i className="fab fa-twitter-square" />
-                  <span className="nav-link-inner--text d-lg-none">
+                  <i className='fab fa-twitter-square' />
+                  <span className='nav-link-inner--text d-lg-none'>
                     Twitter
                   </span>
                 </NavLink>
-                <UncontrolledTooltip delay={0} target="tooltip366258619">
+                <UncontrolledTooltip delay={0} target='tooltip366258619'>
                   Follow us on Twitter
                 </UncontrolledTooltip>
               </NavItem>
               <NavItem>
                 <NavLink
-                  className="nav-link-icon"
-                  href="https://github.com/creativetimofficial?ref=creative-tim"
-                  id="tooltip931502898"
-                  target="_blank"
+                  className='nav-link-icon'
+                  href='https://github.com/creativetimofficial?ref=creative-tim'
+                  id='tooltip931502898'
+                  target='_blank'
                 >
-                  <i className="fab fa-github" />
-                  <span className="nav-link-inner--text d-lg-none">Github</span>
+                  <i className='fab fa-github' />
+                  <span className='nav-link-inner--text d-lg-none'>Github</span>
                 </NavLink>
-                <UncontrolledTooltip delay={0} target="tooltip931502898">
+                <UncontrolledTooltip delay={0} target='tooltip931502898'>
                   Star us on Github
                 </UncontrolledTooltip>
               </NavItem>
-              <NavItem className="d-none d-lg-block ml-lg-4">
+              <NavItem className='d-none d-lg-block ml-lg-4'>
                 <Button
-                  className="btn-neutral btn-icon"
-                  color="default"
-                  href="https://www.creative-tim.com/product/nextjs-argon-dashboard-pro?ref=njsadp-index-navbar"
-                  target="_blank"
+                  className='btn-neutral btn-icon'
+                  color='default'
+                  href='https://www.creative-tim.com/product/nextjs-argon-dashboard-pro?ref=njsadp-index-navbar'
+                  target='_blank'
                 >
-                  <span className="btn-inner--icon">
-                    <i className="fas fa-shopping-cart mr-2" />
+                  <span className='btn-inner--icon'>
+                    <i className='fas fa-shopping-cart mr-2' />
                   </span>
-                  <span className="nav-link-inner--text">Purchase now</span>
+                  <span className='nav-link-inner--text'>Purchase now</span>
                 </Button>
               </NavItem>
             </Nav>
